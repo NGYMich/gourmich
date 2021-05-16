e6de452051e50366eee037e220e4a2d9eb84d593
NGYMich <ngmich95@gmail.com>
2021-05-16 18:27:36 +0200

    modifs for livre recette

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {DeviceDetectorService} from 'ngx-device-detector';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +9,13 @@ import {Component, OnInit} from '@angular/core';
 export class HomeComponent implements OnInit {
   javaFunctionalities: string[] = ['Ajout de la difficulté', 'Modification des temps (int) en (time)', 'Ajout des ingrédients (HashSet ou ArrayList)', 'Ajout des catégories d\'aliments'];
   angularFunctionalities: string[] = ['Ajout de recettes', 'Modification de recettes', 'Recette aléatoire', 'Ajustement des quantités d\'ingrédients en fonction du nombre de personnes', 'Livre des recettes'];
+  isMobile: boolean;
 
   ngOnInit(): void {
-
+    this.isMobile = this.deviceService.isMobile() ? true : false;
   }
 
-  constructor() {
+  constructor(private deviceService: DeviceDetectorService) {
   }
 
 
