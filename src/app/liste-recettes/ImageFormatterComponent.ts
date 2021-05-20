ce943e73776c42b23650ce44b61a33c92de4ad77
NGYMich <ngmich95@gmail.com>
2021-05-20 21:35:39 +0200

    no pic fix

--- src/app/liste-recettes/ImageFormatterComponent.ts
+++ src/app/liste-recettes/ImageFormatterComponent.ts
@@ -1,8 +1,11 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-image-formatter-cell',
-  template: `<img style="border-radius: 40px; border: solid 1px;" width="200" height="300" src=\"{{ params.value }}\">` })
+  template: `<img *ngIf="params.value" style="border-radius: 40px; border: solid 1px;" width="200" height="300" src=\"{{ params.value }}\">  <img *ngIf="!params.value" style="border-radius: 40px; border: solid 1px;" width="200" height="300" src="https://forums.autodesk.com/autodesk/attachments/autodesk/124/166553/1/Personal_Tri_Part_-_01_2018-Apr-27_02-32-37PM-000_CustomizedView28598958894_png_alpha.png">
+
+  `
+})
 
 export class ImageFormatterComponent {
   params: any;
